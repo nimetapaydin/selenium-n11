ee0bc535b7fb3dda903f77f06a67f34ad40166a0
nimetapaydin <nimetapaydin@gmail.com>
2022-01-21 23:49:17 +0300

    Search

--- src/test/java/n11/Search.java
+++ src/test/java/n11/Search.java
@@ -1,9 +1,11 @@
 package n11;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Search {
@@ -31,6 +33,19 @@ public class Search {
         //arama yapma
         WebElement search = driver.findElement(By.xpath("//*[@class='searchBtn']"));
         search.click();
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,450)");
+
+        //WebElement third = driver.findElement(By.cssSelector(".view > ul > li:nth-child(3)"));
+        //third.click();
+
+        Actions action = new Actions(driver);
+
+        WebElement third = driver.findElement(By.xpath("//*[@id=\"view\"]/ul/li[3]"));
+        //third.click();
+        action.doubleClick(third).perform();
+
     }
 
 }
